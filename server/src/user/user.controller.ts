45397ff2d7b85d868886465cc3936e2497354bc4
Ivan Klimovskiy <klimoffsky27@gmail.com>
2025-03-22 22:58:12 +0300

    Добавить контроллер получение профиля

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtGuard } from '@app/auth/guards/jwt.guard';
+import { Request } from 'express';
+import { User } from '@app/user/entities/user.entity';
+import { UserProfileResponseDto } from '@app/user/dto/user-profile.response.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  async getUserProfile(@Req() req: Request): Promise<UserProfileResponseDto> {
+    const { username } = req.user as User;
+    const user = await this.userService.findByUsername(username);
+    return {
+      id: user.id,
+      username: user.username,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+      createdAt: user.createdAt.toString(),
+      updatedAt: user.updatedAt.toString(),
+    };
+  }
 }
