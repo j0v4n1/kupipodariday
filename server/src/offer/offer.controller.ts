7d563e0db957b3d8128afdbd53d444723789f59e
Ivan Klimovskiy <klimoffsky27@gmail.com>
2025-03-31 23:39:17 +0300

    Добавить функции поиска одного или всех предложений

--- server/src/offer/offer.controller.ts
+++ server/src/offer/offer.controller.ts
@@ -6,6 +6,8 @@ import {
   Req,
   UsePipes,
   ValidationPipe,
+  Get,
+  Param,
 } from '@nestjs/common';
 import { OfferService } from './offer.service';
 import { CreateOfferDto } from './dto/create-offer.dto';
@@ -13,12 +15,12 @@ import { JwtGuard } from '@app/auth/guards/jwt.guard';
 import { Request } from 'express';
 import { User } from '@app/user/entities/user.entity';
 
+@UseGuards(JwtGuard)
+@UsePipes(new ValidationPipe())
 @Controller('offers')
 export class OfferController {
   constructor(private readonly offerService: OfferService) {}
 
-  @UseGuards(JwtGuard)
-  @UsePipes(new ValidationPipe())
   @Post()
   async create(
     @Body() createOfferDto: CreateOfferDto,
@@ -28,5 +30,15 @@ export class OfferController {
     const { itemId } = createOfferDto;
     await this.offerService.create(createOfferDto, id, itemId);
     return {};
+  }
+
+  @Get(':id')
+  async findOne(@Param() offer: { id: string }) {
+    return await this.offerService.findOne(Number(offer.id));
+  }
+
+  @Get()
+  async findAll() {
+    return await this.offerService.findAll();
   }
 }
--- server/src/offer/offer.service.ts
+++ server/src/offer/offer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateOfferDto } from '@app/offer/dto/create-offer.dto';
 import { Repository } from 'typeorm';
 import { Offer } from '@app/offer/entities/offer.entity';
@@ -6,6 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserService } from '@app/user/user.service';
 import { WishService } from '@app/wish/wish.service';
 import { Wish } from '@app/wish/entities/wish.entity';
+import AppDataSource from '@app/data-source';
 
 @Injectable()
 export class OfferService {
@@ -32,5 +33,28 @@ export class OfferService {
     offer.item = wish;
 
     return await this.offerRepository.save(offer);
+  }
+
+  async findOfferById(id: number) {
+    const offer = await this.offerRepository.findOne({
+      where: { id },
+      relations: { item: true, user: true },
+    });
+    if (!offer) {
+      throw new HttpException('Предложение не найдено', HttpStatus.NOT_FOUND);
+    }
+    return offer;
+  }
+
+  async findOne(offerId: number) {
+    return await this.findOfferById(offerId);
+  }
+
+  async findAll() {
+    return await AppDataSource.getRepository(Offer)
+      .createQueryBuilder('offer')
+      .leftJoinAndSelect('offer.user', 'user')
+      .leftJoinAndSelect('offer.item', 'wish')
+      .getMany();
   }
 }
